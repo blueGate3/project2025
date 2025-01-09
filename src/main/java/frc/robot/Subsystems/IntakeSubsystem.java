a068665461243698869d14c0f90e731d4880c805
Tick <103855043+TickingEmulator@users.noreply.github.com>
2025-01-08 19:12:48 -0800

    Update IntakeSubsystem.java

--- src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
@@ -1,6 +1,10 @@
 package frc.robot.Subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
 public class IntakeSubsystem extends SubsystemBase {
-  public IntakeSubsystem(){}
+  private final CANSparkMax rightUpperMotor = new CANSparkMax(IntakeConstants.kRightUpperMotor, MotorType.kBrushless);
+  private final CANSparkMax rightLowerMotor = new CANSparkMax(IntakeConstants.kRightLowerMotor, MotorType.kBrushless);
+  private final CANSparkMax leftUpperMotor = new CANSparkMax(IntakeConstants.kLeftUpperMotor, MotorType.kBrushless);
+  private final CANSparkMax leftLowerMotor = new CANSparkMax(IntakeConstants.kLeftLowerMotor, MotorType.kBrushless);
   public void setIntakeState(){}
 }

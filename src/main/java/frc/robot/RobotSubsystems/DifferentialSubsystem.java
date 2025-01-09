8936cf5f27cf72e1d349d95cc02e883e4d8c4285
Tick <103855043+TickingEmulator@users.noreply.github.com>
2025-01-08 16:18:08 -0800

    Create DifferentialSubsystem.java

---
+++ src/main/java/frc/robot/RobotSubsystems/DifferentialSubsystem.java
@@ -0,0 +1,6 @@
+package frc.robot.RobotSubsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+public class DifferentialSubsystem extends SubsystemBase {
+  public DifferentialSubsystem(){}
+  public void setDifferentialState(){}
+}

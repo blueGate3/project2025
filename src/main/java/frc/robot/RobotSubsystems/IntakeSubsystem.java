dd4e6e709fa4059c1ee5fe54f324ba2021f41b71
Tick <103855043+TickingEmulator@users.noreply.github.com>
2025-01-08 18:31:57 -0800

    Update IntakeSubsystem.java

--- src/main/java/frc/robot/RobotSubsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/RobotSubsystems/IntakeSubsystem.java
@@ -1,4 +1,4 @@
-package frc.robot.RobotSubsystems;
+package frc.robot.Subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class IntakeSubsystem extends SubsystemBase {
   public IntakeSubsystem(){}

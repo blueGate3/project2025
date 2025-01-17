32d422d93ffa01849aaa4522b61c2b016a86c8f6
Tick <103855043+TickingEmulator@users.noreply.github.com>
2025-01-16 23:48:30 -0500

    Delete src/main/src/main/src directory

--- src/main/src/main/src/main/java/frc/robot/Subsystems/LoopManager/Loop.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.Subsystems.LoopManager;
-
-public interface Loop {
-    public void register(Loop loop);
-    public void onStart();
-    public void onLoop();
-    public void onStop();
-}

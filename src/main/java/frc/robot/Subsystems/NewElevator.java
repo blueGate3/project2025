3ef8bda2166a4a2487a3733cc3e4b45189ea33a9
blueGate3 <162855123+blueGate3@users.noreply.github.com>
2025-03-24 16:22:31 -0400

    implemented everything needed for sysid hopefully, except for actually running it and i still dk how to get the logs.

--- src/main/java/frc/robot/Subsystems/NewElevator.java
+++ src/main/java/frc/robot/Subsystems/NewElevator.java
@@ -1,23 +1,42 @@
 package frc.robot.Subsystems;
 
+import frc.robot.Robot;
 import frc.robot.Constants.DriveConst;
 import frc.robot.Constants.ElevatorConst;
 
 import com.revrobotics.spark.*;
+
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+
 import com.revrobotics.*;
 import com.revrobotics.spark.config.*;
 import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.SparkBase.*;
+import edu.wpi.first.util.datalog.*;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.system.LinearSystem;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.units.measure.MutDistance;
+import edu.wpi.first.units.measure.MutLinearVelocity;
+import edu.wpi.first.units.measure.MutVoltage;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.sysid.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
-public class NewElevator {
+
+
+public class NewElevator extends SubsystemBase{
   private SparkFlex m_leftMotor;
   private SparkFlex m_rightMotor;
   private SparkFlexConfig m_leftMotorConfig;
@@ -25,20 +44,43 @@ public class NewElevator {
   private SparkClosedLoopController m_Controller;
   private RelativeEncoder m_Encoder;
 
-  // private ElevatorSim m_ElevatorSim = new ElevatorSim(
-  //   ElevatorConst.kV,
-  //   ElevatorConst.kA,
-  //     new DCMotor(12,
-  //     3.65, 
-  //     218, 
-  //     3.54, 
-  //     702.04, //6704 rpm, max for a vortex
-  //     2), 
-  //   0, //starts at floor
-  //   ElevatorConst.L4Height, //max height
-  //   true, 
-  //   ElevatorConst.homePosition, //starting position, this is the bottom of the chute for the cradle. 
-  //   null);
+  //might need none of this
+  //Not entirely sure what these lines do... https://docs.wpilib.org/en/stable/docs/software/basic-programming/java-units.html and https://github.com/wpilibsuite/allwpilib/blob/main/wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/sysidroutine/subsystems/Drive.java
+  //for later... something about not reallocating memory but apparently we need these for data logging idk why.
+  // Mutable holder for unit-safe voltage values, persisted to avoid reallocation.
+  private final MutVoltage m_appliedVoltage = Volts.mutable(0);
+  // Mutable holder for unit-safe linear distance values, persisted to avoid reallocation.
+  private final MutDistance m_distance = Meters.mutable(0);
+  // Mutable holder for unit-safe linear velocity values, persisted to avoid reallocation.
+  private final MutLinearVelocity m_velocity = MetersPerSecond.mutable(0);
+  
+  SysIdRoutine routine = new SysIdRoutine(
+    new SysIdRoutine.Config(),
+    new SysIdRoutine.Mechanism(voltage -> {
+      m_leftMotor.setVoltage(voltage);
+      m_rightMotor.setVoltage(voltage);
+
+    }, log -> {
+      // Record a frame for the left motor.
+      log.motor("Left Elevator Motor: ")
+          .voltage(
+              m_appliedVoltage.mut_replace(
+                  m_leftMotor.get() * RobotController.getBatteryVoltage(), Volts))
+          .linearPosition(m_distance.mut_replace(m_leftMotor.getEncoder().getPosition(), Meters))
+          .linearVelocity(
+              m_velocity.mut_replace(m_leftMotor.getEncoder().getVelocity(), MetersPerSecond));
+      // Record a frame for the right motor.
+      log.motor("Right Elevator Motor: ")
+          .voltage(
+              m_appliedVoltage.mut_replace(
+                  m_rightMotor.get() * RobotController.getBatteryVoltage(), Volts))
+          .linearPosition(m_distance.mut_replace(m_rightMotor.getEncoder().getPosition(), Meters))
+          .linearVelocity(
+              m_velocity.mut_replace(m_rightMotor.getEncoder().getVelocity(), MetersPerSecond));
+
+    }, this)
+    );
+  
   private ElevatorFeedforward m_ElevatorFeedforward = new ElevatorFeedforward(
     ElevatorConst.kS, 
     ElevatorConst.kG, 
@@ -59,11 +101,13 @@ public class NewElevator {
     .inverted(true)
     .smartCurrentLimit(60);
     m_leftMotorConfig.closedLoop
-    .pid(1.2, 0, .4)
+    .pid(ElevatorConst.kP, ElevatorConst.kI, ElevatorConst.kD)
     .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
     .positionWrappingEnabled(true); //may not need or may be actively bad idk
 
-    m_leftMotorConfig.encoder.positionConversionFactor(ElevatorConst.rotationToMeterScaler); //switches us from motor rotations to meters
+    m_leftMotorConfig.encoder
+    .velocityConversionFactor(ElevatorConst.rotationToMeterScaler*60) //times 60 for minutes to seconds, hopefully
+    .positionConversionFactor(ElevatorConst.rotationToMeterScaler); //switches us from motor rotations to meters
     m_leftMotor.getEncoder().setPosition(ElevatorConst.homePosition);
 
     m_rightMotorConfig.follow(ElevatorConst.leftID);
@@ -72,11 +116,28 @@ public class NewElevator {
     m_Controller = m_leftMotor.getClosedLoopController();
     m_leftMotor.configure(m_leftMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
-
   }
 
   public void setPosition() {
 
+  }
+
+  /**
+   * Returns a command that will execute a quasistatic test in the given direction.
+   *
+   * @param direction The direction (forward or reverse) to run the test in
+   */
+  public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
+    return routine.quasistatic(direction);
+  }
+
+  /**
+   * Returns a command that will execute a dynamic test in the given direction.
+   *
+   * @param direction The direction (forward or reverse) to run the test in
+   */
+  public Command sysIdDynamic(SysIdRoutine.Direction direction) {
+    return routine.dynamic(direction);
   }
 
 
